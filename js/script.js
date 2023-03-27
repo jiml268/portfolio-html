901a333beb030f2405fffe8d2eabc2cf77c9b9ec
jiml268 <jimlynch268@gmail.com>
2023-03-26 20:18:53 -0400

    start work on portfolio

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const infotext = document.querySelector('.intro');
+console.log.infotext
+infotext.addEventListener('mouseover', changecolor)
+
+
+
+
+function changecolor(e) {
+    console.log(e.target)
+    console.log(e.target.style)
+    console.log(e.target.className)
+    if (e.target.nodeName !== "SPAN") {
+        return
+    }
+    if (e.target.className !== "chande-color-name") {
+        if (e.target.style.color !== "gray") {
+            e.target.style.color = "gray"
+        } else {
+            e.target.style.color = "black"
+        }
+    } else {
+        if (e.target.style.color !== "red") {
+            e.target.style.color = "red"
+        } else {
+            e.target.style.color = "blue"
+        }
+    }
+}
